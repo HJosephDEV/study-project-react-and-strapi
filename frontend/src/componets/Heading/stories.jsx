239a9dff36a652112d2ec98668eb5fae007136cb
Joseph Haase <joseph.lima@codenapp.com>
2023-03-01 08:28:37 -0300

    Adicionando configurações ao storybook e atualizando o css global

--- frontend/src/componets/Heading/stories.jsx
+++ frontend/src/componets/Heading/stories.jsx
@@ -4,11 +4,25 @@ export default {
     title: 'Heading',
     component: Heading,
     args: {
-        children: 'Não sei'
+        children: 'Não sei',
+        light: false
     },
     argTypes: {
-        children: {type: 'string'}
+        children: {type: 'string'},
+        light: {type: 'boolean'}
+    },
+    parameters: {
+        backgrounds: {
+            default: 'dark'
+        }
     }
 };
 
-export const Template = (args) => <Heading {...args} />;
+export const Light = (args) => <Heading {...args} />;
+export const Dark = (args) => <Heading {...args} light={true}/>;
+
+Light.parameters = {
+    backgrounds: {
+        default: 'light'
+    }
+}
